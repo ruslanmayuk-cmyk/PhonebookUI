233e1293a3ac71f55546133ad802149575876087
ruslanmaiuk <ruslanmayuk@gmail.com>
2026-01-28 21:21:37 +0100

    Fix DataProvider structure and test parameters

---
+++ src/main/java/de/phonebook/data/ContactData.java
@@ -0,0 +1,12 @@
+package de.phonebook.data;
+
+public class ContactData {
+    public static final String NAME = "Ruslan";
+    public static final String LASTNAME = "Maiuk";
+    public static final String PHONE = "1234567890";
+    public static final String EMAIL = "qa@gmail";
+    public static final String ADRESS = "Mulheim";
+    public static final String DESCRIPTION = "QA";
+
+    public static final String INVALID_PHONE = "111";
+}
---
+++ src/main/java/de/phonebook/data/UserData.java
@@ -0,0 +1,7 @@
+package de.phonebook.data;
+
+public class UserData {
+    public static final String EMAIL = "ruslammayuk2@gmail.com";
+    public static final String PASSWORD = "rrrr12AA$";
+
+}
